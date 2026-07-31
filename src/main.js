a717c463fde6e759d06a80ddca331cdad6f3d399
Psydev <blaat2k@mail.com>
2026-07-31 22:22:16 +0200

    add tailwind

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router/index.js'
 
+import './styles.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
